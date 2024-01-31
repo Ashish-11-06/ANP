33a1cab71e98ca1f135b295b844b31929e62527d
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-01-31 18:54:24 +0530

    shift added

---
+++ operator/Shift.java
@@ -0,0 +1,21 @@
+package operator;
+
+public class Shift {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = -10;
+        
+        // Left Shift Operator (<<)
+        System.out.println("Left Shift Operator (<<)");
+        System.out.println("Before shifting: " + num1 + " binary: " + Integer.toBinaryString(num1));
+        System.out.println("After shifting by 2 bits: " + (num1 << 2) + " binary: " + Integer.toBinaryString(num1 << 2));
+        System.out.println();
+        
+        // Right Shift Operator (>>)
+        System.out.println("Right Shift Operator (>>)");
+        System.out.println("Before shifting: " + num1 + " binary: " + Integer.toBinaryString(num1));
+        System.out.println("After shifting by 2 bits: " + (num1 >> 2) + " binary: " + Integer.toBinaryString(num1 >> 2));
+        
+       }
+}
